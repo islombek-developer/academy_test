4600c930c021fcac53fc9376205eeec8fad42643
islombek-develober <islombekbotiraliyev1@gmail.com>
2025-04-09 15:51:14 +0500

    fix

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/islombek/Desktop/imtihon/venv
+set -gx VIRTUAL_ENV /home/islombek/Desktop/academy_test/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
