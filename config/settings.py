adfcd7f057a401536b8065b488f5f273593156ff
islombek-develober <islombekbotiraliyev1@gmail.com>
2025-04-09 16:09:43 +0500

    edit

--- config/settings.py
+++ config/settings.py
@@ -124,12 +124,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = "/static/"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
